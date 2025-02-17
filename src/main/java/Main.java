809075edef895cdaad3ecec557a80fed367251a4
Manuel Simon <info@der-supernerd.de>
2025-02-17 11:03:29 +0100

    DateTimeFormat

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
+    public static void main(String[] args) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(ZonedDateTime.now());
+        System.out.println(localDateTime);
+
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        String nowDateTime = localDateTime.format(dateTimeFormatter);
+        System.out.println(nowDateTime);
+    }
 }
