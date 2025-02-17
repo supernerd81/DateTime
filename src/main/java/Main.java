389840e5ecff3d6462db7b59b731ded2834eba2c
Manuel Simon <info@der-supernerd.de>
2025-02-17 11:09:53 +0100

    DateTimeFormat + 2 Weeks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,16 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
 
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
         LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(ZonedDateTime.now());
-        System.out.println(localDateTime);
+        System.out.println("ZoneDateTame: " + zonedDateTime);
+        System.out.println("LocalDateTime: " + localDateTime);
 
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
         String nowDateTime = localDateTime.format(dateTimeFormatter);
         System.out.println(nowDateTime);
+
+        System.out.println("Plus 2 Wochen: " + zonedDateTime.plusWeeks(2));
     }
 }
